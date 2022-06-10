6db7d842de0617ec3668fada797afbb2f4f6c358
robert-ambroziak-mir <104061335+robert-ambroziak-mir@users.noreply.github.com>
2022-06-10 16:37:21 +0200

    added FullName to User

--- GitFlowSolution/Domain/DTO/User.cs
+++ GitFlowSolution/Domain/DTO/User.cs
@@ -7,5 +7,6 @@ namespace Domain.DTO
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
+        public string FullName { get;set;}
     }
 }
