9b7168056303b51e850c6f85f26beb1ca9ad82fe
robert-ambroziak-mir <robert.ambroziak@makeitright.pl>
2022-06-10 16:43:39 +0200

    replece Fullname to First and last name

--- GitFlowSolution/Domain/DTO/User.cs
+++ GitFlowSolution/Domain/DTO/User.cs
@@ -7,6 +7,7 @@ namespace Domain.DTO
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public string FullName { get;set;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
